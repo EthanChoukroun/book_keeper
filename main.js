6b064ad3a4a342e788a305db126b94a361c9f70d
Ethan Choukroun <echoukroun@seatgeek.com>
2024-03-01 17:41:00 -0800

    First commit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-s
+import express from 'express';
+import axios from 'axios';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(morgan('tiny'));
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
